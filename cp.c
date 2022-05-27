bbec651b3a846a238370e6c77e8855076fdde673
Ayushhh-T <100257355+Ayushhh-T@users.noreply.github.com>
2022-05-27 19:27:22 +0530

    Update cp.c

--- cp.c
+++ cp.c
@@ -1,37 +1,73 @@
 // C program  to check the balanced parenthesis.
-#include<stdio.h>
-int main()
-{
-    char expression[50]; // declaration of char type array
-    int x=0, i=0; // declaration of two integer type variables
-    printf("\nEnter an expression: ");
-    scanf("%s", expression);
-// Scanning the expression until we reach the end of the expression.
- while(expression[i]!= '\0')
-    {
-    // Condition to check the symbol is '('
-     if(expression[i]=='(')
-        {
-            x++;    // incrementing 'x' variable
-        }
-     // condition to check the symbol is ')'
-     else if(expression[i]==')')
-        {
-            x--;   // decrementing 'x' variable
-            if(x<0)
-            break;
-        }
-    i++;       // incrementing 'i' variable.
-    }
-    // Condition to check whether x is equal to 0 or not.
-    if(x==0)
-    {
-        printf("Expression is balanced");
-    }
+#include <stdio.h>
+#include <stdlib.h>
 
-    else
-    {
-        printf("Expression is unbalanced");
-    }
-    return 0;
+#define MAX 100
+
+struct stack {
+  char stck[MAX];
+  int top;
+}s;
+
+void push(char item) {
+  if (s.top == (MAX - 1))
+    printf("Stack is Full\n");
+
+  else {
+    s.top = s.top + 1;
+    s.stck[s.top] = item;
+  }
 }
+
+void pop() {
+  if (s.top == -1)
+    printf("Stack is Empty\n");
+
+  else
+    s.top = s.top - 1;
+}
+
+int checkPair(char val1,char val2){
+    return (( val1=='(' && val2==')' )||( val1=='[' && val2==']' )||( val1=='{' && val2=='}' ));
+}
+
+int checkBalanced(char expr[], int len){
+      
+    for (int i = 0; i < len; i++)  
+    { 
+        if (expr[i] == '(' || expr[i] == '[' || expr[i] == '{')  
+        {  
+          push(expr[i]); 
+        } 
+        else
+        {
+            // exp = {{}}}
+            // if you look closely above {{}} will be matched with pair, Thus, stack "Empty"
+            //but an extra closing parenthesis like '}' will never be matched
+            //so there is no point looking forward
+        if (s.top == -1) 
+            return 0;
+        else if(checkPair(s.stck[s.top],expr[i]))
+        {
+            pop();
+            continue;
+        }
+        // will only come here if stack is not empty
+        // pair wasn't found and it's some closing parenthesis
+        //Example : {{}}(]
+        return 0;
+        }
+    }    
+    return 1; 
+}
+int main() {
+  char exp[MAX] = "({})[]{}";
+  int i = 0;
+  s.top = -1;
+
+  int len = strlen(exp);
+  checkBalanced(exp, len)?printf("Balanced"): printf("Not Balanced"); 
+
+  return 0;
+}
+
